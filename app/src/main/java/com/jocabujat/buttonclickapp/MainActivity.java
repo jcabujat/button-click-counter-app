a394e5fd6b2211e334d7c8304f4861611799c502
Jonathan Cabujat <jonathancabujat@gmail.com>
2020-04-25 18:00:58 +0800

    Update on app function - version 2: - storing text from user input - retaining all data saved when changing phone orientation (portrait -> landscape and vice-versa) - added some logs to confirm android activity lifecycle

--- app/src/main/java/com/jocabujat/buttonclickapp/MainActivity.java
+++ app/src/main/java/com/jocabujat/buttonclickapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.jocabujat.buttonclickapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,32 +12,84 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
-    private Button button;
+    private EditText userInput;
     private TextView textView;
-    private int numTimesClicked = 0;
+    private final String TEXT_CONTENTS = "TextContents";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
-        textView = (TextView) findViewById(R.id.textView);
+        userInput = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        userInput.setText("");
+        Button button = findViewById(R.id.button);
         textView.setText("");
         textView.setMovementMethod(new ScrollingMovementMethod());
         View.OnClickListener ourOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numTimesClicked = numTimesClicked + 1;
-                String result = "The button got tapped " + numTimesClicked + " time";
-                if (numTimesClicked != 1) {
-                    result += "s";
-                }
-                textView.append(result + "\n");
+                String result = userInput.getText().toString();
+                result += "\n";
+                textView.append(result);
+                userInput.setText("");
             }
         };
         button.setOnClickListener(ourOnClickListener);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        outState.putString(TEXT_CONTENTS, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString(TEXT_CONTENTS));
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
+
 }
